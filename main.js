e18a7058972e5032389164a290f0530b36148496
MomoPZ <mompitza@gmail.com>
2014-03-31 18:36:37 +0200

    cleanup

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ define(function (require, exports, module) {
         Dialogs         = brackets.getModule("widgets/Dialogs"),
         EditorManager   = brackets.getModule("editor/EditorManager"),
         ExtensionUtils  = brackets.getModule("utils/ExtensionUtils"),
-        
+
         template        = require("text!html/ckeditor.html"),
         ckjs            = require("ckeditor/ckeditor"),
         ckconfig        = require("ckeditor.config"),
@@ -15,11 +15,11 @@ define(function (require, exports, module) {
         $ck_modal;
 
 
-    
-	/**    
-	 * @private    
-	 * Finds both, the staring and the closing html-tag or when not found an empty object    
-	 * @param {object} cm              Codemirror instance    
+
+	/**
+	 * @private
+	 * Finds both, the staring and the closing html-tag or when not found an empty object
+	 * @param {object} cm              Codemirror instance
 	 * @param {object} startTagToken   {line, pos}  - the token the endtag must be find for
      *
      * returns an object with positions of starting and closing tags:
@@ -36,11 +36,11 @@ define(function (require, exports, module) {
 		}
 		return result;
 	}
-    
-	/**    
-	 * @private    
+
+	/**
+	 * @private
 	 * Finds the first starting tag in a range
-	 * @param {object} cm      Codemirror instance    
+	 * @param {object} cm      Codemirror instance
 	 * @param {object} range   { {line: startline, ch: startPos}, {line: endLine, ch: EndPos}  }
      *
      * returns an object {line, pos} with the position of the starting range
@@ -52,20 +52,20 @@ define(function (require, exports, module) {
 		while (l <= range.end.line) {
 			var tok = _getFirstTagInLine(cm, l);
 			if (tok.start) {
-				return { line: l, ch: tok.start };
+				return {line: l, ch: tok.start};
 			}
 			l++;
 		}
 		return result;
 	}
-	
-    /**    
-	 * @private    
-	 * Finds the last tag in a range. 
-     * To get proper html the result can exceed the given range when a tag in the given range contains a closing tag outside of the range. 
-	 * @param {object} cm      Codemirror instance    
+
+    /**
+	 * @private
+	 * Finds the last tag in a range.
+     * To get proper html the result can exceed the given range when a tag in the given range contains a closing tag outside of the range.
+	 * @param {object} cm      Codemirror instance
 	 * @param {object} range   { {line: startline, ch: startPos}, {line: endLine, ch: EndPos}  }
-     * 
+     *
      * returns an object {line, pos} with the position of the end of the range
 	 */
 	function _getRangeEnd(cm, range) {
@@ -74,13 +74,13 @@ define(function (require, exports, module) {
 		while (l <= range.end.line) {
 			var tok = _getFirstTagInLine(cm, l),
                 tokEndTag = _getMatchingEndTag(cm, {line: l, ch: tok.start});
-            
+
 			if (tokEndTag.end) {
 				if (
 					(tokEndTag.end.line > range.end.line) || //passed end line ??
 					(tokEndTag.end.line == range.end.line && tokEndTag.end.ch >= range.end.ch) //same line but passed end ch ??
 				) {
-					return { line: tokEndTag.end.line, ch: tokEndTag.end.ch };
+					return {line: tokEndTag.end.line, ch: tokEndTag.end.ch};
 
 				}
 			}
@@ -88,11 +88,11 @@ define(function (require, exports, module) {
 		}
 		return result;
 	}
-    
-	/**    
-	 * @private    
-	 * _getFirstTagInLine    
-	 * @param {object} cm  Codemirror instance     
+
+	/**
+	 * @private
+	 * _getFirstTagInLine
+	 * @param {object} cm  Codemirror instance
 	 * @param {int} l      Line
      *
      * returns {object} token with its position {startpos, endpos} when it's a tag else an empty {object}
@@ -114,34 +114,34 @@ define(function (require, exports, module) {
 		return result;
 	}
 
-    /**    
-	 * @private    
-	 * _getTokenAtCursor    
-	 * @param {object} cm              Codemirror instance     
+    /**
+	 * @private
+	 * _getTokenAtCursor
+	 * @param {object} cm              Codemirror instance
 	 * @param {object} {line, pos} pos Cursor position
      *
-     * returns {object} token with it's position {startpos, endpos} 
+     * returns {object} token with it's position {startpos, endpos}
 	 */
 	function _getTokenAtCursor(cm, pos) {
 
 		return cm.getTokenAt({line: pos.line, ch: pos.ch});
 	}
-    
-    /**    
-	 * @private    
-	 * _nextToken    
-	 * @param {object} cm              Codemirror instance     
+
+    /**
+	 * @private
+	 * _nextToken
+	 * @param {object} cm              Codemirror instance
 	 * @param {object} {line, pos} pos Cursor position
      *
-     * returns {object} token with it's position {startpos, endpos} 
+     * returns {object} token with it's position {startpos, endpos}
 	 */
 	function _nextToken(cm, pos) {
 
 		return cm.getTokenAt({line: pos.line, ch: pos.ch + 1});
 	}
 
-	/**    
-	 * @private    
+	/**
+	 * @private
 	 * This stupid function makes an attempt to find the paths of css-files which are included in the html
      * (a proper method would compare the paths with ProjectManager.getAllFiles(ProjectManager.getLanguageFilter("css")) )
      * ignores paths starting "/" or "../" etc.
@@ -150,18 +150,18 @@ define(function (require, exports, module) {
      * returns {Array} with absolute Filepath of included css-files
 	 */
 	function _extractCSSFiles(hostEditor) {
-		
+
 		var docFolder = hostEditor.document.file.parentPath,
             docContent = hostEditor.document.getText(),
             cssLinks = [],
             cssPatt = /<link\s+(?:[^>]*?\s+)?href="([^"]*)"/g,
             links = docContent.match(cssPatt);
-        
+
 		if (links) {
 			for (var i = 0; i < links.length; i++) {
-				
+
 				links[i] = links[i].substr(links[i].indexOf('href="')+6);
-				links[i] = links[i].substr(0, links[i].lastIndexOf('"')); 
+				links[i] = links[i].substr(0, links[i].lastIndexOf('"'));
 				var ext = links[i].substr(links[i].lastIndexOf(".") +1);
 				if (links[i].substr(0, 1).match(/[A-Z,a-z]/g) && ext == "css") {
 					cssLinks.push(docFolder + links[i])
@@ -171,18 +171,17 @@ define(function (require, exports, module) {
 		return cssLinks;
 	}
 
-	/**    
-	 * @private    
-	 * Loads the dialog with ckeditor an the content to edit    
-	 * @param {type} hostEditor     
-	 * @param {type} content   Content to edit    
-	 * @param {type} range     Range to replace in hostEditor    
-	 * @param {type} indent    Indent for in ckeditor edited content     
+	/**
+	 * @private
+	 * Loads the dialog with ckeditor an the content to edit
+	 * @param {type} hostEditor
+	 * @param {type} content   Content to edit
+	 * @param {type} range     Range to replace in hostEditor
 	 */
-	function _loadCk (hostEditor, content, range, indent) {
+	function _loadCk (hostEditor, content, range) {
 
 		Dialogs.showModalDialogUsingTemplate(template, false);
-        
+
         //first the cancel-button - so if something goes wrong we at least can leave the dialog
         $ck_modal = $(".ckdialog.instance");
 		$ck_modal.find("#ck_cancel").click( function () {
@@ -191,13 +190,13 @@ define(function (require, exports, module) {
 			}
             Dialogs.cancelModalDialogIfOpen("ckdialog", "ck_cancel");
         });
-        
+
 		//prepare the config for CKeditor
 		var cssFilesConfig = {};
 		cssFilesConfig.contentsCss = _extractCSSFiles(hostEditor)
         var config = $.extend({}, ckConfig, cssFilesConfig);
-        
-		CKEDITOR.replace("ckeditor1", config); 
+
+		CKEDITOR.replace("ckeditor1", config);
 		CKEDITOR.on("instanceReady", function(ev) {
 			CKEDITOR.instances.ckeditor1.setData(content);
 			CKEDITOR.instances.ckeditor1.focus();
@@ -219,8 +218,8 @@ define(function (require, exports, module) {
 
 	}
 
-    /**    
-     * Kick-off - calculates the range to edit and init dialog with ckeditor... 
+    /**
+     * Kick-off - calculates the range to edit and init dialog with ckeditor...
      */
     function bracketsCkeditor() {
 
@@ -231,12 +230,12 @@ define(function (require, exports, module) {
         if (hostEditor.getLanguageForSelection().getId() !== "html") {
             return null;
         }
-        
+
         //Conntent to edit:
         var range,
             content = "",
-            sel = hostEditor.getSelection(),
-            indent = hostEditor.document.getRange( { line: sel.start.line, ch: 0 }, { line: sel.start.line, ch: sel.start.ch } );
+            sel = hostEditor.getSelection();
+
         //no selection -> empty editor
         if (sel.start.line === sel.end.line && sel.start.ch === sel.end.ch) {
 			range = sel;
@@ -247,21 +246,16 @@ define(function (require, exports, module) {
                 rangeEnd = _getRangeEnd(cm, sel);
 
 			if(rangeStart.line && rangeEnd.line) {
-
-				indent = hostEditor.document.getRange( { line: rangeStart.line, ch: 0 }, rangeStart );
 				range = {start: rangeStart, end: rangeEnd};
 				content = hostEditor.document.getRange(rangeStart, rangeEnd);
 			}
-			else { 
+			else {
                 //invalid range - probably no tag in selection -> empty editor
 				range = {start: sel.start, end: sel.start};
 			}
 		}
-		
-		//remove none whitespace from indent
-		indent = indent.replace(/\S/g, " ");
-		
-		_loadCk(hostEditor, content, range, indent);
+
+		_loadCk(hostEditor, content, range);
 
     }
 
